907ce097930b9053a8bd73e78f29c6794146761a
Justin Koh <justinkoh@rogers.com>
2019-09-18 23:04:22 -0400

    doggy

---
+++ Assets/Scripts/Dog/DogCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogCamera : MonoBehaviour
+{
+    public Transform dog_transform;
+    [Range(0.01f, 1.0f)]
+    public float smoothing = 0.5f;
+
+    private Vector3 camera_offset;
+
+    private void Start()
+    {
+        camera_offset = this.transform.position - dog_transform.position;
+    }
+
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+        Vector3 new_position = dog_transform.position + camera_offset;
+        this.transform.position = Vector3.Slerp(this.transform.position, new_position, smoothing);
+        this.transform.LookAt(dog_transform);
+    }
+}
