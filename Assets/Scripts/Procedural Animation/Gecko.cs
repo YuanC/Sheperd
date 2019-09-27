e287cf9497aaee75bddd67fee9bc8cc33b485462
Justin Koh <justinkoh@rogers.com>
2019-09-26 20:06:29 -0400

    procedural animation lizard

---
+++ Assets/Scripts/Procedural Animation/Gecko.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gecko : MonoBehaviour
+{
+    // The target we are going to track
+    [SerializeField] Transform target;
+    // A reference to the gecko's neck
+    [SerializeField] Transform headBone;
+    [SerializeField] Transform leftEyeBone;
+    [SerializeField] Transform rightEyeBone;
+
+    [SerializeField] float headMaxTurnAngle;
+    [SerializeField] float headTrackingSpeed;
+    [SerializeField] float eyeTrackingSpeed;
+
+    [SerializeField] float leftEyeMaxYRotation;
+    [SerializeField] float leftEyeMinYRotation;
+    [SerializeField] float rightEyeMaxYRotation;
+    [SerializeField] float rightEyeMinYRotation;
+
+    [SerializeField] LegStepper frontLeftLegStepper;
+    [SerializeField] LegStepper frontRightLegStepper;
+    [SerializeField] LegStepper backLeftLegStepper;
+    [SerializeField] LegStepper backRightLegStepper;
+
+    void Awake()
+    {
+        StartCoroutine(LegUpdateCoroutine());
+    }
+
+    void LateUpdate()
+    {
+        HeadUpdate();
+        EyeUpdate();
+    }
+
+    private void HeadUpdate()
+    {
+        // Store the current head rotation since we will be resetting it
+        Quaternion currentLocalRotation = headBone.localRotation;
+        // Reset the head rotation so our world to local space transformation will use the head's zero rotation. 
+        // Note: Quaternion.Identity is the quaternion equivalent of "zero"
+        headBone.localRotation = Quaternion.identity;
+
+        Vector3 targetWorldLookDir = target.position - headBone.position;
+        Vector3 targetLocalLookDir = headBone.parent.InverseTransformDirection(targetWorldLookDir);
+
+        // Apply angle limit
+        targetLocalLookDir = Vector3.RotateTowards(
+            Vector3.forward,
+            targetLocalLookDir,
+            Mathf.Deg2Rad * headMaxTurnAngle, // Note we multiply by Mathf.Deg2Rad here to convert degrees to radians
+            0 // We don't care about the length here, so we leave it at zero
+        );
+
+        // Get the local rotation by using LookRotation on a local directional vector
+        Quaternion targetLocalRotation = Quaternion.LookRotation(targetLocalLookDir, Vector3.up);
+
+        // Apply smoothing
+        headBone.localRotation = Quaternion.Slerp(
+            currentLocalRotation,
+            targetLocalRotation,
+            1 - Mathf.Exp(-headTrackingSpeed * Time.deltaTime)
+        );
+    }
+
+    private void EyeUpdate()
+    {
+        // Note: We use head position here just because the gecko doesn't look so great when cross eyed.
+        // To make it relative to the eye itself, subtract the eye's position instead of the head's.
+        Quaternion targetEyeRotation = Quaternion.LookRotation(
+            target.position - headBone.position, // toward target
+            transform.up
+        );
+
+        leftEyeBone.rotation = Quaternion.Slerp(
+            leftEyeBone.rotation,
+            targetEyeRotation,
+            1 - Mathf.Exp(-eyeTrackingSpeed * Time.deltaTime)
+        );
+
+        rightEyeBone.rotation = Quaternion.Slerp(
+            rightEyeBone.rotation,
+            targetEyeRotation,
+            1 - Mathf.Exp(-eyeTrackingSpeed * Time.deltaTime)
+        );
+
+        float leftEyeCurrentYRotation = leftEyeBone.localEulerAngles.y;
+        float rightEyeCurrentYRotation = rightEyeBone.localEulerAngles.y;
+
+        // Move the rotation to a -180 ~ 180 range
+        if (leftEyeCurrentYRotation > 180)
+        {
+            leftEyeCurrentYRotation -= 360;
+        }
+        if (rightEyeCurrentYRotation > 180) 
+        {
+            rightEyeCurrentYRotation -= 360;
+        }
+
+        // Clamp the Y axis rotation
+        float leftEyeClampedYRotation = Mathf.Clamp(
+            leftEyeCurrentYRotation,
+            leftEyeMinYRotation,
+            leftEyeMaxYRotation
+        );
+        float rightEyeClampedYRotation = Mathf.Clamp(
+            rightEyeCurrentYRotation,
+            rightEyeMinYRotation,
+            rightEyeMaxYRotation
+        );
+
+        // Apply the clamped Y rotation without changing the X and Z rotations
+        leftEyeBone.localEulerAngles = new Vector3(
+            leftEyeBone.localEulerAngles.x,
+            leftEyeClampedYRotation,
+            leftEyeBone.localEulerAngles.z
+        );
+        rightEyeBone.localEulerAngles = new Vector3(
+            rightEyeBone.localEulerAngles.x,
+            rightEyeClampedYRotation,
+            rightEyeBone.localEulerAngles.z
+        );
+    }
+    // Only allow diagonal leg pairs to step together
+    IEnumerator LegUpdateCoroutine()
+    {
+        // Run forever
+        while (true)
+        {
+            // Try moving one diagonal pair of legs
+            do
+            {
+                frontLeftLegStepper.TryMove();
+                backRightLegStepper.TryMove();
+                // Wait a frame
+                yield return null;
+            
+            // Stay in this loop while either leg is moving.
+            // If only one leg in the pair is moving, the calls to TryMove() will let
+            // the other leg move if it wants to.
+            } while (backRightLegStepper.Moving || frontLeftLegStepper.Moving);
+
+            // Do the same thing for the other pair
+            do
+            {
+                frontRightLegStepper.TryMove();
+                backLeftLegStepper.TryMove();
+                yield return null;
+            } while (backLeftLegStepper.Moving || frontRightLegStepper.Moving);
+        }
+    }
+}
